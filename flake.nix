b012f46e8985f5285b89f17cbba23b69562062b5
RobWalt <robwalter96@gmail.com>
2024-03-04 18:43:38 +0100

    chore(setup): setup flake
    Authored-by: Aviac <aviac@mailbox.org>

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ { flake-parts, ... }:
+    flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      {
+        systems = inputs.flake-utils.lib.defaultSystems;
+        imports = [ ./nix/bevy.nix ];
+        perSystem = { pkgs, config, ... }: {
+          devShells.default = config.devShells.stable;
+          formatter = pkgs.nixpkgs-fmt;
+        };
+      };
+}
