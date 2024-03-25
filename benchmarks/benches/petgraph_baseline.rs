246c22250997f3370d1f533a2ce39bfd22d71826
aviac <aviac@mailbox.org>
2024-03-25 08:55:02 +0100

    refactor to workspace layout

---
+++ benchmarks/benches/petgraph_baseline.rs
@@ -0,0 +1,14 @@
+// use criterion::{black_box, criterion_group, criterion_main, Criterion};
+// use petgraph::Graph;
+//
+// fn create_graph<const N: usize>() -> Graph<(), ()> {
+//     todo!()
+// }
+//
+// fn criterion_benchmark(c: &mut Criterion) {
+//     c.bench_function("create a basic graph", |b| b.iter(|| todo!()));
+// }
+//
+// criterion_group!(benches, criterion_benchmark);
+// criterion_main!(benches);
+fn main() {}
