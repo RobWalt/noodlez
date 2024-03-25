4b0da32562a395cc88dbb844b0eba04a0df50560
aviac <aviac@mailbox.org>
2024-03-25 09:19:27 +0100

    add binary for visual tests

--- noodlez/src/bin/main.rs
+++
@@ -1,6 +0,0 @@
-#[allow(unused)]
-use noodlez::prelude::*;
-
-fn main() {
-    println!("Hello, world!");
-}
---
+++ spaghetti/src/main.rs
@@ -0,0 +1,8 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, WorldInspectorPlugin::default()))
+        .run();
+}
